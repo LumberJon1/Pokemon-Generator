bc44c825b17a76f7f56bf5b47748fab1a1a4043d
Jonathan W <jwilliams12117@gmail.com>
2022-01-25 21:39:14 -0700

    Added API query to generate a list of pokemon types

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,22 @@
 //Selector variables
 
-//arrays to hold types and pokemon numbers
+//array to hold types
+var types = [];
 
-//onclick event for the button
+//Get the array of different types from API
+var getTypes = function() {
+    apiURL = "https://pokeapi.co/api/v2/type/?limit=20";
 
-//function...
+    fetch(apiURL).then(function(response) {
+        response.json().then(function(pokeTypes) {
+
+            //Push each item to global types array
+            for (var i = 0; i < pokeTypes.results.length; i++) {
+                types.push(pokeTypes.results[i].name);
+            }
+        });
+    });
+}
 
 
 
@@ -33,32 +45,53 @@ var generatePokemon= function(pokemon) {
     var sprite = $("<img>").attr("src", pokeSprite).addClass("poke-img");
     pokemonDisplayEl.append(sprite);
 
-    //If there are no types currently being displayed, create them
+    //Clear any previous types listed
 
+    //Display either one or two types for the pokemon
+    var typeList = [];
 
-    //Otherwise, modify them
+    if (pokeTypes.length === 1) {
+        //Only generate one type
+        typeList.push(pokeTypes[0].type.name);
+    }
+    else {
+        //generate two types
+        for (var i = 0; i < pokeTypes.length; i++) {
+            typeList.push(pokeTypes[i].type.name);
+        }
+    }
+
+    //Create elements from the typeList
+    var typesDiv = $("<div>").addClass("d-flex justify-content-center col-4 mx-auto mb-3 mt-1");
+
+    for (var i = 0; i < typeList.length; i++) {
+        typesDiv.append("<span>").text(typeList[i])
+            .addClass("badge badge-pill badge-success");
+    };
+
+    console.log("typeList:", typeList);
+    pokemonSectionEl.append(typesDiv);
 
     return;
 
 };
 
 var requestPokemon = function(number) {
-    console.log("Searching API for Pokemon number"+number);
     apiURL = "https://pokeapi.co/api/v2/pokemon/"+number+"?limit=100";
 
     fetch(apiURL).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
             generatePokemon(data);
         });
     });
 
 }
 
+getTypes();
+
 randomizeButtonEl.addEventListener("click", function() {
     
     //Use a random number to pick a pokemon
     var randomPokeIndex = Math.floor(Math.random() * 1000);
-    console.log(randomPokeIndex);
     requestPokemon(randomPokeIndex);
 });
